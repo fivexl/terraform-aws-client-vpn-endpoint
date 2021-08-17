8da7d8ead8155365b2702c16f995af7dbdd928dd
Andrey Devyatkin <andrey.devyatkin@fivexl.io>
2021-08-17 13:22:00 +0200

    update outputs to return endpoint
    Signed-off-by: Andrey Devyatkin <andrey.devyatkin@fivexl.io>

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,10 @@ output "security_group_vpc_id" {
   value = aws_security_group.this.vpc_id
 }
 
+output "ec2_client_vpn_endpoint_id" {
+  value = aws_ec2_client_vpn_endpoint.this_sso.id
+}
+
+output "ec2_client_vpn_endpoint_arn" {
+  value = aws_ec2_client_vpn_endpoint.this_sso.arn
+}
