2d287bb1f1c86f6805450f286d1d3d0887fbc025
Vladimir Samoylov <cageyv.dev@gmail.com>
2022-04-25 23:22:19 +0700

    remove key_algorithm

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ resource "tls_private_key" "this" {
 }
 
 resource "tls_self_signed_cert" "this" {
-  key_algorithm   = tls_private_key.this.algorithm
   private_key_pem = tls_private_key.this.private_key_pem
   subject {
     common_name = var.tls_subject_common_name
