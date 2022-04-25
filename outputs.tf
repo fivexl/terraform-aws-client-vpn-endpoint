d9f330a19b25dc27925a18116f2224b162039f0d
Vladimir Samoylov <cageyv.dev@gmail.com>
2022-04-25 23:25:56 +0700

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,34 @@
 output "security_group_id" {
-  value = aws_security_group.this.id
+  description = "A map of tags to add to all resources"
+  value       = aws_security_group.this.id
 }
 
 output "security_group_name" {
-  value = aws_security_group.this.name
+  description = "Name of the security group"
+  value       = aws_security_group.this.name
 }
 
 output "security_group_description" {
-  value = aws_security_group.this.description
+  description = "Security group description"
+  value       = aws_security_group.this.description
 }
 
 output "security_group_vpc_id" {
-  value = aws_security_group.this.vpc_id
+  description = "VPC ID"
+  value       = aws_security_group.this.vpc_id
 }
 
 output "ec2_client_vpn_endpoint_id" {
-  value = aws_ec2_client_vpn_endpoint.this_sso.id
+  description = "The ID of the Client VPN endpoint"
+  value       = aws_ec2_client_vpn_endpoint.this_sso.id
 }
 
 output "ec2_client_vpn_endpoint_arn" {
-  value = aws_ec2_client_vpn_endpoint.this_sso.arn
+  description = "The ARN of the Client VPN endpoint"
+  value       = aws_ec2_client_vpn_endpoint.this_sso.arn
 }
 
 output "ec2_client_vpn_network_associations" {
-  value = aws_ec2_client_vpn_network_association.this_sso
+  description = "Network associations for AWS Client VPN endpoint"
+  value       = aws_ec2_client_vpn_network_association.this_sso
 }
